5d031de560ae910996fd8907765871f2c01db316
KalachevSerg <carcazz@rambler.ru>
2024-12-22 16:54:24 +0300

    Factorial of a number

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let res = 1; 
+    
+    if(n === 0)
+        return 1;
+    for (let i = 2; i <= n; i++) 
+        res = res * i; 
+    return res; 
 }
+
+    console.log ( factorial(0) );
+    console.log ( factorial(1) );
+    console.log ( factorial(3) );
+    console.log ( factorial(5) );
