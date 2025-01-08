b11889ef315a563b1042a600124ba09c5990e7f8
KalachevSerg <carcazz@rambler.ru>
2025-01-08 16:44:44 +0300

    convert the entire string to lowercase, and then check if it contains the desired substrings

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
+
+alert( checkSpam('1XbeT now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam("innocent rabbit") );
